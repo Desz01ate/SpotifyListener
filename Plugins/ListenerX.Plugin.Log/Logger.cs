e3bba84b6365eb7a12228b59b419eb8ba10fe9ca
Chanvut.B <kunvutloveza@hotmail.com>
2021-01-20 08:44:26 +0700

    Add plugin implementation example.

---
+++ Plugins/ListenerX.Plugin.Log/Logger.cs
@@ -0,0 +1,28 @@
+﻿using Listener.Core.Framework.Players;
+using Listener.Core.Framework.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenerX.Plugin.Log
+{
+    public class Logger : IListenerPlugin
+    {
+        const string DIR = "logs";
+        const string PATH = "log.txt";
+        public Logger()
+        {
+            Directory.CreateDirectory(DIR);
+        }
+        public void OnTrackChanged(IPlayerHost player)
+        {
+            using (var sw = File.AppendText(Path.Combine(DIR, PATH)))
+            {
+                sw.WriteLine($"[{DateTime.Now}] {player.Track}\n");
+            }
+        }
+    }
+}
